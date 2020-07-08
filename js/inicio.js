4fe016f6fcb84e76dc5549552ec9fd52a3ae2e1a
Devison Deiró <devisondeiro@hotmail.com.br>
2020-07-08 13:21:22 -0300

    :recycle: refactor: fix number values defaults For some reason, the values on p or div tags don't change even if we explicitly change with textContent or innerHTML I didn't get too deep, but seems like a bug Instead, I'll use some inputs, and set the default values with js doing this, I always get the changed value

--- js/inicio.js
+++ js/inicio.js
@@ -1,17 +1,24 @@
-// addEventListener('click', (e) => {});
-const $setas = {
-    cima: document.querySelectorAll(".seta-cima"),
-    baixo: document.querySelectorAll(".seta-baixo"),
-    limites: [25, 5, 4]
-}
-
 /* "Mas não seria mais fácil colocar direto em uma constante
  do que fazer um destructuring depois? É mais código!"
  
  R: estou testando a funcionalidade.
  */
+const $setas = {
+    cima: document.querySelectorAll(".seta-cima"),
+    baixo: document.querySelectorAll(".seta-baixo"),
+    limites: [25, 5, 3]
+}
 const { cima, baixo } = $setas;
+
+// Valores padrões
 const $valores = document.querySelectorAll(".etapa__numero");
+let valor_trabalho = $valores[0];
+let valor_pausa = $valores[1];
+let valor_sessao = $valores[2];
+valor_trabalho.defaultValue = 25;
+valor_pausa.defaultValue = 5;
+valor_sessao.defaultValue = 3;
+
 // Tempo limite de cada campo
 const [trabalho, pausa, sessao] = $setas.limites;
 cima.forEach( ( seta, indice ) => {
@@ -19,8 +26,8 @@ cima.forEach( ( seta, indice ) => {
     seta.addEventListener('click', _ => {
         const valor = $valores[indice];
         const limite = [trabalho, pausa, sessao];
-        if (valor.textContent < limite[indice]){
-            valor.textContent++;
+        if (valor.value < limite[indice]){
+            valor.value++;
         }
     })
 })
@@ -30,8 +37,8 @@ baixo.forEach( ( seta, indice ) => {
     seta.addEventListener('click', _ => {
         const valor = $valores[indice];
         const limite = 0;
-        if (valor.textContent > limite){
-            valor.textContent--;
+        if (valor.value > limite){
+            valor.value--;
         }
     })
 })
