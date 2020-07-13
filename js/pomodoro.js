6692dbf48b1b5ac188d4a38c368a489cec4ddfdb
Devison Deiró <devisondeiro@hotmail.com.br>
2020-07-13 11:12:43 -0300

    :sparkles: feat: finish temporizer implementation

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -3,16 +3,33 @@ $inicio_botao.addEventListener('click' , _ => window.open("/views/inicio.html", 
 
 let valor_trabalho = sessionStorage.getItem("trabalho");
 
-let minutes = 3;
-let seconds = new Date;
-seconds.getSeconds();
+let minutos = 3;
+let segundos = 59;
+let temporizador;
 
-// function countdown () {}
+/* 
+Essa função irá implementar a cada iteração "i = 1".
+Caso o número em i seja maior, o código assume que o código
+dentro do for terá que ser feito i = n vezes.
+O que pode prejudicar a saída, já que ele executaria
+Ex caso i = 3:
+3:59 
+3:58
+3:57
+de uma só vez.
+*/
+for (let i = 0; i < 1; i++) {
+	let segundos_contados = segundos;
+	let minutos_contados = minutos;
 
-while ( minutes >= 0 ) {
-
-	for ( seconds = 59; seconds >= 0; seconds--) {
-		console.log(`${minutes}:${seconds}`);
-	}
-	minutes--;
+	temporizador = setInterval( _ => {
+		console.log(`${minutos_contados}:${segundos_contados}`);
+		segundos_contados--;
+			if (segundos_contados < 0) {
+				minutos_contados--;
+				segundos_contados = segundos;
+			} if (minutos_contados === 0 && segundos_contados === 0) {
+				clearInterval(temporizador);
+			}
+	},1000); 
 }
