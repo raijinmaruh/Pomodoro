5a7133447cba6fa5608d4a871dddf8ee1cc2d260
Devison Deiró <devisondeiro@hotmail.com.br>
2020-07-16 11:43:16 -0300

    :bug: fix: fix problem in the countdown end the clock never get in 0:00, staying in 0:01

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,4 +1,5 @@
 // Diretamente do stack overflow
+// Função para fazer com que o número de zeros a esqueda seja suficiente
 Number.prototype.pad = function(tamanho = 2) {
   var string = String(this);
   while (string.length < (tamanho || 2)) {string = "0" + string;}
@@ -8,36 +9,38 @@ Number.prototype.pad = function(tamanho = 2) {
 const $inicio_botao = document.querySelector(".clock__botao");
 $inicio_botao.addEventListener('click' , _ => window.open("/views/inicio.html", "_self"));
 const temporizador_principal = document.querySelector(".clock__numero");
+const inicial = document.querySelector(".clock__start");
 let valor_trabalho = sessionStorage.getItem("trabalho");
 
 temporizador_principal.textContent = `${valor_trabalho}:00`;
 
 /* 
 Essa função irá implementar a cada iteração "i = 1".
-Caso o número em i seja maior, o código assume que o código
+Caso o número em i seja maior, o loop assume que o código
 dentro do for terá que ser feito i = n vezes.
-O que pode prejudicar a saída, já que ele executaria
-Ex caso i = 3:
+A saída seria executada três no caso abaixo.
+Ex: caso i = 3:
 3:59 
 3:58
 3:57
 de uma só vez.
 */
 
-function decrementar(minutos) {
-	const SEGUNDOS = 3;
+function decrementar(minutos = 3) {
+	const SEGUNDOS = 60;
 		for (let i = 0; i < 1; i++) {
 		let segundos_contados = SEGUNDOS;
 		let minutos_contados = minutos;
-
 		let temporizador = setInterval( _ => {
-			temporizador_principal.textContent = `${minutos_contados}:${segundos_contados.pad()}`;
 			segundos_contados--;
-				if (segundos_contados < 0) {
+			temporizador_principal.textContent = `${minutos_contados}:${segundos_contados.pad()}`;
+				if (minutos_contados === 0 && segundos_contados === 0) {
+					temporizador_principal.textContent;
+					clearInterval(temporizador);
+				}
+				if (segundos_contados === 0) {
 					minutos_contados--;
 					segundos_contados = SEGUNDOS;
-				} if (minutos_contados === 0 && segundos_contados === 0) {
-					clearInterval(temporizador);
 				}
 		},1000);
 	}
