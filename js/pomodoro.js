fdef47ded7128d5c2be2c49260fb0a9a3e7dd6dd
Devison Deiró <devisondeiro@hotmail.com.br>
2020-07-21 18:55:10 -0300

    :sparkles: feat: add function to change decrease state

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,6 @@
+const $botao_inicio = document.querySelector(".clock__botao");
+$botao_inicio.addEventListener('click' , _ => window.open("/views/inicio.html", "_self"));
+
 // Diretamente do stack overflow
 // Função para fazer com que o número de zeros a esqueda seja suficiente
 Number.prototype.pad = function(tamanho = 2) {
@@ -7,42 +10,49 @@ Number.prototype.pad = function(tamanho = 2) {
 }
 
 const temporizador_principal = document.querySelector(".clock__numero");
-const iniciar = document.querySelector(".clock__start");
-const $inicio_botao = document.querySelector(".clock__botao");
-$inicio_botao.addEventListener('click' , _ => window.open("/views/inicio.html", "_self"));
-let valor_trabalho = sessionStorage.getItem("trabalho");
+const botao_iniciar = document.querySelector(".clock__start");
+let ativo = document.querySelector(".botao__inicia");
+let pausado = document.querySelector(".botao__pausa");
+const valor_trabalho = sessionStorage.getItem("trabalho");
+let estado = true;
 
 temporizador_principal.textContent = `${valor_trabalho}:00`;
-iniciar.addEventListener('click', decrementar); 
-/* 
-Essa função irá implementar a cada iteração "i = 1".
-Caso o número em i seja maior, o loop assume que o código
-dentro do for terá que ser feito i = n vezes.
-A saída seria executada três no caso abaixo.
-Ex: caso i = 3:
-3:59 
-3:58
-3:57
-de uma só vez.
-*/
+
+botao_iniciar.addEventListener('click', decrementar);
 
 function decrementar() {
+	estado = false;
+	mudarEstado(estado);
 	let minutos = valor_trabalho - 1;
 	const SEGUNDOS = 60;
-		for (let i = 0; i < 1; i++) {
-		let segundos_contados = SEGUNDOS;
-		let minutos_contados = minutos;
-		let temporizador = setInterval( _ => {
-			segundos_contados--;
-			temporizador_principal.textContent = `${minutos_contados}:${segundos_contados.pad()}`;
-				if (minutos_contados === 0 && segundos_contados === 0) {
-					temporizador_principal.textContent;
-					clearInterval(temporizador);
-				}
-				if (segundos_contados === 0) {
-					minutos_contados--;
-					segundos_contados = SEGUNDOS;
-				}
-		},1000);
+		
+	let segundos_contados = SEGUNDOS;
+	let minutos_contados = minutos;
+	let temporizador = setInterval( _ => {
+		segundos_contados--;
+		temporizador_principal.textContent = `${minutos_contados}:${segundos_contados.pad()}`;
+			if (minutos_contados === 0 && segundos_contados === 0) {
+				temporizador_principal.textContent;
+				clearInterval(temporizador);
+			}
+			if (segundos_contados === 0) {
+				minutos_contados--;
+				segundos_contados = SEGUNDOS;
+			}
+	},1000);
+}
+
+function pausar(){
+	
+}
+
+function mudarEstado(estadoAtual) {
+	let escondido = 'escondido';
+	if (estado) {
+		ativo.classList.remove(escondido);
+		pausado.classList.add(escondido);
+	} else {
+		ativo.classList.add(escondido);
+		pausado.classList.remove(escondido);
 	}
 }
